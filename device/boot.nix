b7dbb33bcbb1b35667322b663494f2b38cc654e2
NicoOtLo <nicolasoterolopez@gmail.com>
2026-05-06 17:42:46 +0200

    kernel zen, cpuFreqGovernor schedutil, zram, etc

--- device/boot.nix
+++ device/boot.nix
@@ -1,9 +1,22 @@
 { pkgs, ... }:
 
 {
-  boot.loader.grub.enable = false;
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    loader.grub.enable = false;
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages_latest;
+
+    kernelParams = [
+      "loglevel=4"
+      "debug"
+      "initcall_debug"
+    ];
+  };
+
+  networking.hostName = "Numancia";
+
+  powerManagement.cpuFreqGovernor = "schedutil";
+  # performance / powesave / schedutil
 }
--- device/networking.nix
+++ device/networking.nix
@@ -1,6 +1,1 @@
-{
-  networking = {
-    hostName = "numantia";
 
-  };
-}
